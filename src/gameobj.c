f62704502f67926b06e3f0d9fd2af9687037f320
Igor Morgado <morgado.igor@gmail.com>
2019-02-02 18:34:11 -0200

    Added makefile

---
+++ src/gameobj.c
@@ -0,0 +1,84 @@
+#include <stdbool.h>
+#include <stdlib.h>
+
+#include "gameobj.h"
+#include "phys.h"
+
+
+gameobj * gameobj__init(void)
+{
+    gameobj* self;
+    self = malloc(sizeof *self);
+
+    self->size = 1.0;
+
+    point3f* axis;
+    axis = malloc(sizeof *axis);
+    self->axis = axis;
+
+    point3f* position;
+    position = malloc(sizeof *position);
+    self->position = position;
+
+    point3f* velocity;
+    velocity = malloc(sizeof *velocity);
+    self->velocity = velocity;
+
+    self->acceleration = 0.0;
+
+    self->angle = 0.0;
+    self->angVelocity = 0.0;
+
+    sphere3f* boundSphere;
+    boundSphere = malloc(sizeof *boundSphere);
+    self->boundSphere = boundSphere;
+
+    self->active = false;
+    self->lifeTimer = 0;
+    self->type = 0;
+    self->objFlags = OBJ_RESET;
+
+    return self;
+}
+
+
+void gameobj__destroy(gameobj *self)
+{
+    free(self->boundSphere);
+    free(self->velocity);
+    free(self->position);
+    free(self->axis);
+    free(self);
+}
+
+
+void gameobj__update(gameobj* self, float dt)
+{
+    accelerate(self->velocity, self->acceleration, dt);
+    move(self->position, self->velocity, dt);
+    rotate(&(self->angle), self->angVelocity, dt);
+
+    self->position->z = 0.0f;
+
+    if((self->objFlags & OBJ_NOTIMER) == 0)
+    {
+        self->lifeTimer -= dt;
+        if(self->lifeTimer < 0.0f) {
+            self->active = false;
+        }
+    }
+}
+
+
+point3f* gameobj__unit_vector_facing(gameobj* self)
+{
+    //Returns the direction unit vector
+    return NULL;
+}
+
+
+point3f* gameobj__unit_vector_velocity(gameobj* self)
+{
+    //Returns the velocity unit vector
+    return NULL;
+}
---
+++ src/main.h
@@ -0,0 +1,16 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <errno.h>
+#include <string.h>
+
+#include "phys.h"
+#include "gameobj.h"
+#include "ship.h"
+
+#endif
+
