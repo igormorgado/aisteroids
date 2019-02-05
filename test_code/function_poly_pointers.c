b1a8804c5c7d7b936aba56d974efef13893ab4b4
Igor Morgado <morgado.igor@gmail.com>
2019-02-05 00:29:45 -0200

    Added make file to test code

--- test_code/function_poly_pointers.c
+++ test_code/function_poly_pointers.c
@@ -1,29 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 
+/* **************************************************
+ * Euclidean geometry
+ * **************************************************/
 struct point { float x; float y; };
-
-struct poligon
-{
-    int sides;
-    struct point vertex[20];                // No more than 20 sided polygon
-    float (*area)(struct poligon p);
-    float (*perimeter)(struct poligon p);
-    void (*action)(void);
-};
-
-
-struct triangle { struct poligon; float (*height)(void);
-};
-
-
-struct square
-{
-    struct poligon;
-    float (*height)(void);
-};
-
 
 inline float distance(struct point p1, struct point p2);
        
@@ -32,47 +15,92 @@ float distance(struct point p1, struct point p2)
     return sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
 }
 
-float perimeter(struct poligon P)
+
+/* **************************************************
+ * POLYGON
+ * **************************************************/
+
+struct polygon
 {
-    int i;
-    float p = 0;
+    int sides;
+    struct point vertex[20];                // No more than 20 sided polygon
+    float (*area)(struct polygon p);
+    float (*perimeter)(struct polygon *p);
+    void (*action)(void);
+};
 
-    for (i = 0; i < P.sides - 1; i++)
-    {    
-        p += distance(P.vertex[i], P.vertex[i+1]);
-    }
-    p += distance(P.vertex[i], P.vertex[0]);
+float perimeter(struct polygon *P);
 
-    return p;
+
+struct polygon * polygon_init(int sides)
+{
+    struct polygon *poly;
+    poly = malloc(sizeof *poly);
+    poly->sides = sides;
+    poly->perimeter = perimeter;
+    return poly;
 }
 
+void polygon_destroy(struct polygon * poly)
+{
+    free(poly);
+}
+
+float polygon_perimeter(struct polygon *p)
+{
+    return p->perimeter(p);
+}
+
+/* Polygon Functions */
+float perimeter(struct polygon * poly)
+{
+    float p = 0;
+    for (int i = 0; i < poly->sides - 1; i++)
+        p += distance(poly->vertex[i], poly->vertex[i+1]);
+    return p + distance(poly->vertex[poly->sides - 1], poly->vertex[0]);
+}
+
+
+/* **************************************************
+ * TRIANGLE
+ * **************************************************/
+struct triangle { 
+    struct polygon; 
+    float (*height)(void);
+};
+
+/* **************************************************
+ * SQUARE
+ * **************************************************/
+struct square
+{
+    struct polygon;
+    float (*height)(void);
+};
+
+
+/* **************************************************
+ * MAIN CODE
+ * **************************************************/
 int main(void)
 {
-    struct poligon p1;
-    
+    // Method 1
+    struct polygon p1;
     p1.sides = 3;
-    //
-    // Three different ways to assign values to a struct
     p1.vertex[0] = (struct point){ .x = 0, .y = 0 };
     p1.vertex[1] = (struct point){ .5, 1 };
     p1.vertex[2].x = 1;
     p1.vertex[2].y = 0;
-
     p1.perimeter = &perimeter;
+    printf("P1 %.2f\n", p1.perimeter(&p1));
 
-    printf("P1 %.2f\n", p1.perimeter(p1));
 
-
-    struct poligon *p2;
-    p2 = malloc(sizeof *p2);
-    p2->sides = 3;
-    // Is there any way to assign all three vertex at same time?
-    p2->vertex = (struct point) { (struct point){ 0, 0 },
-                                  (struct point){.5, 1 },
-                                  (struct point){ 1, 0 }
-    };
-    p2->perimeter = &perimeter;
-    printf("P2 %.2f\n", p2->perimeter(*p2));
+    // Method 2
+    struct polygon *p2 = polygon_init(3);
+    struct point t[3] = { {0, 0}, {.5, 1}, {1, 0}};
+    memcpy(p2->vertex, t, sizeof(t));
+    printf("P2 %.2f\n", polygon_perimeter(p2));
+    polygon_destroy(p2);
 
     return 0;
 }
